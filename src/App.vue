0650d1a0ca38b72d6751ba4f821aad6ffe8fabde
stephanieraymos <stephanieraymos@gmail.com>
2022-08-20 22:44:21 -0700

    Super cool typing effect with self object on image

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <typing-and-obj-pic />
+</template>
+
+<script setup>
+import TypingAndObjPic from './components/TypingAndObjPic.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothTypingAndObjPicing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TypingAndObjPic.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="text">Really cool thingy.</div>
+    <div class="code-container">
+      <pre class="code">
+<span class="var-highlight">const</span> self = {
+    name: <span class="string-highlight">'Stephanie'</span>,
+    occupation: <span class="string-highlight">'Bleep Blooper'</span>,
+    location: <span class="string-highlight">'Earth'</span>,
+    hobbies:[
+        <span class="string-highlight">'Biking'</span>,
+        <span class="string-highlight">'Coding'</span>,
+        <span class="string-highlight">'Playing Guitar'</span>
+    ];
+}; </pre>
+    </div>
+    <img src="../assets/self-blue.png" alt="" class="image" />
+    <div class="window-outline"></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+}
+.text {
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: 0.15em solid orange; /* The typwriter cursor */
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: 0.17em; /* Adjust as needed */
+  animation: typing 1.5s steps(40, end), blink-caret 0.75s step-end infinite;
+}
+
+.image {
+  width: 100vw;
+}
+.code-container {
+  position: absolute;
+  color: white;
+  top: 25%;
+  left: 20%;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 15%;
+  }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: orange;
+  }
+}
+</style>
