bd071f1042124cd08e36cd18851c881651bf7c7e
stephanieraymos <stephanieraymos@gmail.com>
2022-08-21 20:44:13 -0700

    Dynamic image inside window

--- src/components/TransWindow.vue
+++ src/components/TransWindow.vue
@@ -1,37 +1,86 @@
 <template>
   <div class="container">
+    <div class="form-container">
+      <form enctype="multipart/form-data">
+        <label for="img">
+          Click here to change the image
+          <input type="file" id="img" name="img" required />
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
     <!-- WINDOW BOX -->
-    <div class="window">
-      <div class="window-top-bar">
+
+    <div id="window">
+      <div id="window-top-bar">
         <div class="buttons">
           <span class="red dot"></span>
           <span class="yellow dot"></span>
           <span class="green dot"></span>
         </div>
       </div>
+      <img
+        id="preview"
+        src="../assets/self-green.png"
+        alt="self-pic"
+        class="image"
+      />
     </div>
     <!-- END WINDOW BOX -->
   </div>
 </template>
 
-<script setup></script>
+<script>
+window.onload = function () {
+  const files = document.querySelectorAll("input[type=file]");
+  files[0].addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        document.getElementById("preview").setAttribute("src", e.target.result);
+      };
+      reader.readAsDataURL(this.files[0]);
+    }
+  });
+};
+</script>
 
 <style lang="scss" scoped>
 .container {
   background: var(--color-dark-blue);
   height: 100vh;
-  .window {
-    position: relative;
-    top: 20%;
-    left: 50%;
-    transform: translateX(-50%);
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .form-container {
+    position: absolute;
+    top: 10%;
+    color: white;
+  }
+  .image {
+    width: 100%;
+    height: 92%;
+    object-fit: cover; // crop
+    object-position: 25% 25%; // centering the crop
+  }
+  input {
+    display: none;
+  }
+  label {
+    cursor: pointer;
+  }
+  #window {
+    position: absolute;
+    // top: 20%;
+    // left: 50%;
     border: var(--color-white) 5px solid;
     width: 70vw;
     height: 50vh;
     box-shadow: 0 3px 10px whitesmoke;
   }
-
-  .window-top-bar {
+  #window-top-bar {
     background: var(--color-white);
     height: 8%;
     margin: 0 -1px; // Takes care of tiny blue line on left and right of bar
@@ -78,6 +127,11 @@
     }
     .green {
       left: 45px;
+    }
+  }
+  @media only screen and (min-width: 1200px) {
+    .window {
+      height: 70vh;
     }
   }
 }
